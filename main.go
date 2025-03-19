7f0188e025ee0169db5e565d07252835328ccb2d
Ishara-Peiris <peirisbhid.21@uom.lk>
2025-03-19 23:56:59 +0530

    syntatic suger

--- main.go
+++ main.go
@@ -4,6 +4,19 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Print("hello world")
+	var EventName = "Earth conference"
+	const TotalTicket = 100
+	var availableTicket int = -45
+
+	fmt.Println("Hi,welcome to",EventName )
+	fmt.Println("you have total number of tickets",TotalTicket,"still available",availableTicket)
+	fmt.Printf("hii,welcome to %v\n",EventName)
+
+	var userName string
+	fmt.Printf("username is %v",userName)
+	fmt.Printf("the type of name variable is %T",userName)
+
+	userEmail:="ishrapeiris2001@gmail.com"
+	fmt.Print(userEmail)
 
 }
