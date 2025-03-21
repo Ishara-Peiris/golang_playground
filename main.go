3dbe4ca50482773aeecbface6d84c5dcfb835887
Ishara-Peiris <peirisbhid.21@uom.lk>
2025-03-21 22:25:19 +0530

    server

--- main.go
+++ main.go
@@ -1,68 +1,29 @@
-
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main(){
-	var EventName = "Earth conference"
-	const TotalTicket = 100
-	var availableTicket int = 45
 
-	fmt.Println("Hi,welcome to",EventName )
+	
+	http.HandleFunc("/hello", greetUser)
 
+	http.ListenAndServe(":8000",nil)
 
-	fmt.Println("you have total number of tickets",TotalTicket,"still available",availableTicket)
-	fmt.Printf("hii,welcome to %v\n",EventName)
-
-	var userName string
-	fmt.Printf("username is %v",userName)
-	fmt.Printf("the type of name variable is %T",userName)
-
-	userEmail:="ishrapeiris2001@gmail.com"
-	fmt.Print(userEmail)
-var firstName string
-var emailAddress string
-var userTicket int
-fmt.Print("enter your first name ")
-	fmt.Scan(&firstName)
-
- 
- 
- fmt.Scan(&emailAddress)
- fmt.Print("enter how many tickets you order")
- fmt.Scan(&userTicket)
-availableTicket=availableTicket-userTicket
-
- fmt.Printf("hi, %v  your %v received and your email:",firstName,emailAddress)
- fmt.Print("available ticket amout is",availableTicket)
-
- var userArray[50]string
- userArray[1]=firstName
- fmt.Print(userArray[1])
-
- var userslice[]string
- userslice=append(userslice, userEmail)
- fmt.Print(userslice)
- fmt.Print(len(userArray))
-
-var taskOne = "getup"
-taskTwo := "meditate"
-taskThree := "reading"
- var todoArray = []string{taskOne,taskTwo,taskThree}
-
- for _,todoItem := range(todoArray){
-	fmt.Printf("%v\n",todoItem)
- }
-fmt.Println()
- 	printTask(todoArray)
-	println()
-	println()
- 	printTask(todoArray)
-	println()
-	todoArray = addTask(todoArray,"goooooooooo")
-	println()
-	printTask(todoArray)
+	readNumber()
 }
+func greetUser (writer http.ResponseWriter, request *http.Request) {
+	var greet = "hello ishara aaaaaaaaaaaaaaa"
+	fmt.Fprintf(writer, greet) 
+} 
+
+func readNumber(){
+	println("isharaaaaaaaaaaaaaaa")
+}
+
+
 func printTask(taskArray[]string){
 	for index,taskItem := range(taskArray){
 		fmt.Printf("%v. %v",index+1,taskItem)
@@ -76,6 +37,8 @@ func printTask(taskArray[]string){
 	 return updatedTask
 
 }
+
+
 
 
 
