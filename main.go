5c058a40d5c2efe834fdb0af4daf30d41c9438e9
Ishara-Peiris <peirisbhid.21@uom.lk>
2025-03-21 22:49:03 +0530

    show-items

--- main.go
+++ main.go
@@ -5,15 +5,21 @@ import (
 	"net/http"
 )
 
+
+var todoItems = []string{"getup","meditate","reading"}
+
 func main(){
 
 	
 	http.HandleFunc("/hello", greetUser)
 
+	http.HandleFunc("/show-items", showItems)
+
 	http.ListenAndServe(":8000",nil)
 
 	readNumber()
 }
+
 func greetUser (writer http.ResponseWriter, request *http.Request) {
 	var greet = "hello ishara aaaaaaaaaaaaaaa"
 	fmt.Fprintf(writer, greet) 
@@ -29,6 +35,13 @@ func printTask(taskArray[]string){
 		fmt.Printf("%v. %v",index+1,taskItem)
 		println()
 	}
+ }
+
+ func showItems(writer http.ResponseWriter , request *http.Request){
+for _ , task := range todoItems {
+	fmt.Fprintf(writer, task)
+
+}
  }
 
  func addTask(taskArray[] string,newTask string ) [] string{
